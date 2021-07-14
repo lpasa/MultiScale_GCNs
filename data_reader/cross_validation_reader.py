1e2b24fa2b1ffd14f2e2692ec8c979253f877a85
lpasa <pasaluca89@gmai.com>
2021-07-14 12:16:51 +0200

    minor updates

--- data_reader/cross_validation_reader.py
+++ data_reader/cross_validation_reader.py
@@ -12,33 +12,6 @@ from utils.utils_methods import get_graph_diameter
 
 rnd_state = np.random.RandomState(seed(1))
 
-
-def collate_batch(batch):
-    '''
-    Creates a batch of same size graphs by zero-padding node features and adjacency matrices up to
-    the maximum number of nodes in the CURRENT batch rather than in the entire dataset.
-    Graphs in the batches are usually much smaller than the largest graph in the dataset, so this method is fast.
-    :param batch: batch in the PyTorch Geometric format or [node_features*batch_size, A*batch_size, label*batch_size]
-    :return: [node_features, A, graph_support, N_nodes, label]
-    '''
-    B = len(batch)
-    N_nodes = [len(batch[b].x) for b in range(B)]
-    C = batch[0].x.shape[1]
-
-    N_nodes_max = int(np.max(N_nodes))
-
-    graph_support = torch.zeros(B, N_nodes_max)
-    A = torch.zeros(B, N_nodes_max, N_nodes_max)
-    x = torch.zeros(B, N_nodes_max, C)
-    for b in range(B):
-        x[b, :N_nodes[b]] = batch[b].x
-        A[b].index_put_((batch[b].edge_index[0], batch[b].edge_index[1]), torch.Tensor([1]))
-        graph_support[b][:N_nodes[b]] = 1  # mask with values of 0 for dummy (zero padded) nodes, otherwise 1
-
-    N_nodes = torch.from_numpy(np.array(N_nodes)).long()
-    labels = torch.from_numpy(np.array([batch[b].y  for b in range(B)])).long()
-    return [x, A, graph_support, N_nodes, labels]
-
 def split_ids(ids, folds=10):
     n = len(ids)
     stride = int(np.ceil(n / float(folds)))
@@ -74,40 +47,15 @@ def getcross_validation_split(dataset_path='~/Dataset/', dataset_name='MUTAG', n
     for fold_id in range(n_folds):
         loaders = []
         for split in [train_ids, test_ids, valid_ids]:
-            # print(torch.from_numpy((train_ids if split.find('train') >= 0 else test_ids)[fold_id]))
-            # print("---")
+
             gdata = dataset[torch.from_numpy(split[fold_id])]
 
             loader = DataLoader(gdata,
                                 batch_size=batch_size,
                                 shuffle=True,
-                                num_workers=4)#,
-                                #collate_fn=collate_batch)
+                                num_workers=4)
             loaders.append(loader)
         splits.append(loaders)
         # print("---")
 
     return splits #0-train, 1-test, 2-valid
-
-if __name__ == '__main__':
-    # cv_splits=getcross_validation_split(n_folds=10)
-    dataset_path = '~/Dataset/'
-    dataset_name = 'PTC_MR'
-    n_folds = 10
-    batch_size=32
-    max_k=3
-    cv_splits=getcross_validation_split(dataset_path, dataset_name, n_folds,
-                              batch_size, K=max_k)
-
-    split_1=cv_splits[2]
-    train=split_1[0]
-    test=split_1[1]
-    valid=split_1[2]
-    for batch in train:
-
-        for i in range(max_k):
-            print(i,batch[f'x{i}'])
-        input("DEBUG")
-    # print(len(train))
-    # print(len(test))
-    # print(len(valid))
--- utils/utils_methods.py
+++ utils/utils_methods.py
@@ -19,43 +19,6 @@ def printParOnFile(test_name, log_dir, par_list):
         f.write(str(key)+": \t"+str(value))
         f.write("\n")
 
-def reconstruct_graphs_from_batch(batch):
-    current_graph=0
-    batch_x=[]
-    batch_reco=[]
-
-    for batch_index,x in zip(batch.batch,batch.x):
-
-        if batch_index != current_graph:
-            batch_reco.append((batch_x, batch.y[batch_index-1].item()))
-            current_graph +=1
-            batch_x=[]
-
-        batch_x.append(x.tolist())
-    batch_reco.append((batch_x, batch.y[batch_index - 1].item()))
-    #def edge_index
-    current_graph = 0
-    current_graph_edge_0 = []
-    current_graph_edge_1 = []
-    edge_index_reco=[]
-    zero_graph_edge=0
-
-    for edge_0,edge_1 in zip(batch.edge_index[0],batch.edge_index[1]):
-        if batch.batch[edge_0] != current_graph: #essendo tutti disgiunti basta controllare che uno dei due edge sia nel grafo
-            edge_index_reco.append([current_graph_edge_0,current_graph_edge_1])
-            current_graph+=1
-            current_graph_edge_0 = []
-            current_graph_edge_1 = []
-            zero_graph_edge=min(edge_0,edge_1)
-        current_graph_edge_0.append(edge_0.item()-zero_graph_edge)
-        current_graph_edge_1.append(edge_1.item()-zero_graph_edge)
-
-    edge_index_reco.append([current_graph_edge_0, current_graph_edge_1])
-    graphs=[]
-    for graph, graph_edge in zip(batch_reco,edge_index_reco):
-
-        graphs.append(Data(x=torch.tensor(graph[0]),edge_index=torch.tensor(graph_edge),y=torch.tensor([graph[1]])))
-    return graphs
 
 def get_graph_diameter(data):
     networkx_graph = to_networkx(data).to_undirected()
@@ -66,39 +29,3 @@ def get_graph_diameter(data):
         sub_graph_diam.append(diameter(sub_g))
     data.diameter=max(sub_graph_diam)
     return data
-
-def get_diameter(graph):
-    networkx_graph = to_networkx(graph).to_undirected()
-
-    sub_graph_list = [networkx_graph.subgraph(c) for c in connected_components(networkx_graph)]
-    sub_graph_diam = []
-    for sub_g in sub_graph_list:
-        sub_graph_diam.append(diameter(sub_g))
-    return max(sub_graph_diam)
-
-def get_max_graphs_k(dataset):
-    max_graphs_k=0
-    for graph in dataset:
-            k_g=graph.diameter
-            if k_g>max_graphs_k:
-                max_graphs_k=k_g
-    return max_graphs_k
-
-def get_A(graph):
-
-    adjacency_indexes = graph.edge_index
-    A_rows = adjacency_indexes[0]
-    A_cols = adjacency_indexes[1]
-    A_data = [1] * A_rows.shape[0]
-
-    indices = torch.stack((A_rows, A_cols),dim=0)
-    i = torch.LongTensor(indices)
-    v = torch.FloatTensor(A_data)
-
-    A=torch.sparse.FloatTensor(i, v, torch.Size((graph.x.shape[0],graph.x.shape[0])))
-    return A.to_dense()
-
-
-if __name__ == '__main__':
-    a={"test1":1,"test2":2}
-    printParOnFile("test","./",a)
